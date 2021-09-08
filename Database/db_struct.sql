b3e1ae859473e86c583197bfd1d365b5e47027ac
Ean Maloney <eanmaloney@gmail.com>
2021-09-08 18:49:46 -0400

    Prepped things for SQL integration

--- Database/db_struct.sql
+++ Database/db_struct.sql
@@ -1,81 +1,67 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+﻿-- Adapted from export from QuickDBD: https://www.quickdatabasediagrams.com/
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/GAwvkn
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "eada" (
-    "unitid" VARCHAR(6)   NOT NULL,
-    "institution_name" VARCHAR   NOT NULL,
-    "addr1_txt" VARCHAR   NOT NULL,
-    "addr2_txt" VARCHAR   NOT NULL,
-    "city_txt" VARCHAR   NOT NULL,
-    "state_cd" VARCHAR(2)   NOT NULL,
-    "zip_text" VARCHAR(9)   NOT NULL,
+CREATE TABLE eada(
+    "unitid" VARCHAR(6) PRIMARY KEY,
+    "institution_name" VARCHAR NOT NULL,
+    "addr1_txt" VARCHAR,
+    "addr2_txt" VARCHAR,
+    "city_txt" VARCHAR,
+    "state_cd" VARCHAR(2),
+    "zip_text" VARCHAR(9),
     "ClassificationCode" VARCHAR(2)   NOT NULL,
     "classification_name" VARCHAR   NOT NULL,
-    "ClassificationOther" VARCHAR   NOT NULL,
+    "ClassificationOther" VARCHAR,
     "EFMaleCount" INT   NOT NULL,
     "EFFemaleCount" INT   NOT NULL,
     "EFTotalCount" INT   NOT NULL,
-    "sector_cd" VARCHAR(2)   NOT NULL,
-    "sector_name" VARCHAR   NOT NULL,
-    "RECRUITEXP_MEN" BIGINT   NOT NULL,
-    "RECRUITEXP_TOTAL" BIGINT   NOT NULL,
-    "GRND_TOTAL_REVENUE" BIGINT   NOT NULL,
-    "GRND_TOTAL_EXPENSE" BIGINT   NOT NULL,
-    "OPEXPPERTEAM_MEN_Baseball" BIGINT   NOT NULL,
-    "OPEXPPERTEAM_MEN_Bskball" BIGINT   NOT NULL,
-    "REV_MEN_Baseball" BIGINT   NOT NULL,
-    "REV_MEN_Bskball" BIGINT   NOT NULL,
-    "EXP_MEN_Baseball" BIGINT   NOT NULL,
-    "EXP_MEN_Bskball" BIGINT   NOT NULL,
-    CONSTRAINT "pk_eada" PRIMARY KEY (
-        "unitid"
-     )
+    "sector_cd" VARCHAR(2),
+    "sector_name" VARCHAR,
+    "RECRUITEXP_MEN" BIGINT,
+    "RECRUITEXP_TOTAL" BIGINT,
+    "GRND_TOTAL_REVENUE" BIGINT,
+    "GRND_TOTAL_EXPENSE" BIGINT,
+    "OPEXPPERTEAM_MEN_Baseball" BIGINT,
+    "OPEXPPERTEAM_MEN_Bskball" BIGINT,
+    "REV_MEN_Baseball" BIGINT,
+    "REV_MEN_Bskball" BIGINT,
+    "EXP_MEN_Baseball" BIGINT,
+    "EXP_MEN_Bskball" BIGINT,
+	"lat" NUMERIC(6,3)   NOT NULL,
+	"lon" NUMERIC(6,3)   NOT NULL
 );
 
 CREATE TABLE "nba_geo" (
-    "team" VARCHAR   NOT NULL,
+    "team" VARCHAR  PRIMARY KEY,
     "city" VARCHAR   NOT NULL,
     "state_cd" VARCHAR(2)   NOT NULL,
     "region" VARCHAR(3)   NOT NULL,
     "stadium_lat" NUMERIC(6,3)   NOT NULL,
-    "stadium_long" NUMERIC(6,3)   NOT NULL,
-    CONSTRAINT "pk_nba_geo" PRIMARY KEY (
-        "team"
-     )
+    "stadium_long" NUMERIC(6,3)   NOT NULL
 );
 
 CREATE TABLE "mlb_geo" (
-    "team" VARCHAR   NOT NULL,
+    "team" VARCHAR   PRIMARY KEY,
     "city" VARCHAR   NOT NULL,
     "state_cd" VARCHAR(2)   NOT NULL,
     "region" VARCHAR(3)   NOT NULL,
     "stadium_lat" NUMERIC(6,3)   NOT NULL,
-    "stadium_long" NUMERIC(6,3)   NOT NULL,
-    CONSTRAINT "pk_mlb_geo" PRIMARY KEY (
-        "team"
-     )
+    "stadium_long" NUMERIC(6,3)   NOT NULL
 );
 
 CREATE TABLE "school_geo" (
-    "unitid" VARCHAR(6)   NOT NULL,
-    "institution_name" VARCHAR   NOT NULL,
-    "school_lat" NUMERIC(6,3)   NOT NULL,
-    "school_long" NUMERIC(6,3)   NOT NULL,
+    "unitid" VARCHAR(6)  REFERENCES eada(unitid),
+    "institution_name" VARCHAR,
+	"state_cd" VARCHAR(2) NOT NULL,
+    "lat" NUMERIC(6,3)   NOT NULL,
+    "lon" NUMERIC(6,3)   NOT NULL,
     "region" VARCHAR(3)   NOT NULL,
-    "nearest_nba" VARCHAR   NOT NULL,
-    "dist_nearest_nba" NUMERIC(6,1)   NOT NULL,
-    "nearest_mlb" VARCHAR   NOT NULL,
-    "dist_nearest_mlb" NUMERIC(6,1)   NOT NULL
+    "nearest_mlb" VARCHAR REFERENCES mlb_geo("team"),
+    "nearest_mlb_dist" NUMERIC(6,1)   NOT NULL,
+    "nearest_nba" VARCHAR REFERENCES nba_geo("team"),
+    "nearest_nba_dist" NUMERIC(6,1)   NOT NULL
 );
 
-ALTER TABLE "school_geo" ADD CONSTRAINT "fk_school_geo_unitid_institution_name" FOREIGN KEY("unitid", "institution_name")
-REFERENCES "eada" ("unitid", "institution_name");
-
-ALTER TABLE "school_geo" ADD CONSTRAINT "fk_school_geo_nearest_nba" FOREIGN KEY("nearest_nba")
-REFERENCES "nba_geo" ("team");
-
-ALTER TABLE "school_geo" ADD CONSTRAINT "fk_school_geo_nearest_mlb" FOREIGN KEY("nearest_mlb")
-REFERENCES "mlb_geo" ("team");
 
