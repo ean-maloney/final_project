97c1b6bf9d7bea995857b4806f4a79541a5ac419
Ean Maloney <eanmaloney@gmail.com>
2021-09-16 10:26:03 -0400

    Fixed ignore

---
+++ Dashboard/app.py
@@ -0,0 +1,17 @@
+import os
+from flask import(
+    Flask,
+    render_template,
+    jsonify,
+    request,
+    redirect)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run() 
